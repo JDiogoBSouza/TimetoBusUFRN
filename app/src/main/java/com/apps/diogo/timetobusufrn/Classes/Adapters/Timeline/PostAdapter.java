e11f4958c81ed204a3fea4a933f7a0fe7692eb52
Diogo Souza <di0g0-dg@hotmail.com>
2017-12-03 18:12:48 -0300

    Mudança do botão "C" para um Expandable ListView.

--- app/src/main/java/com/apps/diogo/timetobusufrn/Classes/Adapters/Timeline/PostAdapter.java
+++ app/src/main/java/com/apps/diogo/timetobusufrn/Classes/Adapters/Timeline/PostAdapter.java
@@ -94,7 +94,7 @@ public class PostAdapter extends ArrayAdapter<Post>
             @Override
             public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id)
             {
-                setListViewHeight(parent, groupPosition);
+                setParentListViewHeight(parent, groupPosition);
                 return false;
             }
         });
@@ -102,40 +102,40 @@ public class PostAdapter extends ArrayAdapter<Post>
         return rowView;
     }
     
-    private void setListViewHeight(ExpandableListView listView,
-                                   int group) {
+    private void setParentListViewHeight(ExpandableListView listView, int group)
+    {
         ExpandableListAdapter listAdapter = (ExpandableListAdapter) listView.getExpandableListAdapter();
         int totalHeight = 0;
-        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(),
-                View.MeasureSpec.EXACTLY);
-        for (int i = 0; i < listAdapter.getGroupCount(); i++) {
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.EXACTLY);
+        
+        for (int i = 0; i < listAdapter.getGroupCount(); i++)
+        {
             View groupItem = listAdapter.getGroupView(i, false, null, listView);
             groupItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
             
             totalHeight += groupItem.getMeasuredHeight();
             
-            if (((listView.isGroupExpanded(i)) && (i != group))
-                    || ((!listView.isGroupExpanded(i)) && (i == group))) {
-                for (int j = 0; j < listAdapter.getChildrenCount(i); j++) {
-                    View listItem = listAdapter.getChildView(i, j, false, null,
-                            listView);
+            if (((listView.isGroupExpanded(i)) && (i != group)) || ((!listView.isGroupExpanded(i)) && (i == group)))
+            {
+                for (int j = 0; j < listAdapter.getChildrenCount(i); j++)
+                {
+                    View listItem = listAdapter.getChildView(i, j, false, null, listView);
                     listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
                     
                     totalHeight += listItem.getMeasuredHeight();
-                    
                 }
             }
         }
         
         ViewGroup.LayoutParams params = listView.getLayoutParams();
-        int height = totalHeight
-                + (listView.getDividerHeight() * (listAdapter.getGroupCount() - 1));
+        int height = totalHeight + (listView.getDividerHeight() * (listAdapter.getGroupCount() - 1));
+        
         if (height < 10)
             height = 200;
+        
         params.height = height;
         listView.setLayoutParams(params);
         listView.requestLayout();
-        
     }
     
     private void setImagemEmpresa(int idEmpresa, ImageView imageOnibus)
